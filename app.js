7f292d2cf6d13eae258e255c7cc6dc3911ae771d
Abhijith Dameruppala <74647983+abh1jith@users.noreply.github.com>
2023-07-12 00:20:18 +0530

    Connected to Database

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-var posts = [];
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/blogwebsite");
+
+const PostSchema = mongoose.Schema({
+  title: String,
+  body: String
+});
+
+const Post = mongoose.model("post", PostSchema );
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -30,31 +40,51 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-  const post = {
+  const post_1 = new Post({
   title : req.body.titleInput,
   body: req.body.postInput
-};
-  posts.push(post);
+});
+
+  post_1.save().then(function(){
+    console.log("Post has been added")
+  });
   res.redirect("/");  
-  // var post = {title : req.body.titleInput, body: req.body.postInput};
 })
 
 app.get("/", function(req, res){
-  res.render( "home",{startContent: homeStartingContent, posts: posts});
+  Post.find({})
+    .then(function(allPosts){
+      console.log(allPosts);
+      res.render( "home",{startContent: homeStartingContent, posts: allPosts});
+
+    })
+    .catch(function(err){
+      console.log(err);
+      res.send("Some is wrong with the database connection.");
+    });
 });
 
 app.get("/posts/:topic", function(req, res){
-  posts.forEach(function(e){
-    if(_.lowerCase(e.title) === _.lowerCase(req.params.topic)){
-      console.log("Match found!");
-      res.render( "post", {title: e.title, body: e.body});
-    }
-    else{
-      console.log("Match NOT found");
-    }
-})
-  // console.log(req.params.topic);
+  Post.find({})
+    .then(function(posts){
+      posts.forEach(function(e){
+        if(_.lowerCase(e.title) === _.lowerCase(req.params.topic)){
+          console.log("Match found!");
+          res.render( "post", {title: e.title, body: e.body});
+        }
+        else{
+          console.log("Match NOT found");
+          res.send("There is no such post in the database");
+
+        } 
+    })
+  })
+  .catch(function(err){
+    console.log(err);
+  });
+  
 });
+  // console.log(req.params.topic);
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
